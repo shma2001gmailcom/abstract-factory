f302f393f913d9a0ae77d23186b2db9fe295f97e
misha <shma2001@gmail.com>
2017-07-24 18:20:41 +0300

    add build script

--- bash/build.sh
+++ bash/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mvn='/usr/share/maven/bin/mvn'
+mvn='/home/misha/workspace/apache-maven-3.5.0/bin/mvn'
 app='abstract-factory'
-
+cd ../
 ${mvn} clean install
-cd ../target
+cd target
 chmod 'a+x' ${app}.jar
 java -jar ${app}.jar
